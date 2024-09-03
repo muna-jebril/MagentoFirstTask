66aeeee2276a8dfc78cdddc65630d52d16de7c2b
Muna-jebril <Muna.jebril1995@hotmail.com>
2024-09-03 19:14:30 +0300

    test2and3

--- src/lesson7/Magento.java
+++ src/lesson7/Magento.java
@@ -1,5 +1,6 @@
 package lesson7;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 
@@ -7,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -21,6 +23,7 @@ public class Magento {
 	public void MyBeforeSetup() {
 		
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
 
 		
 		
@@ -30,10 +33,10 @@ public class Magento {
 
 	
 	
-	@Test(invocationCount = 10)
-	public void AddRandomItem() throws InterruptedException {
+	@Test(invocationCount = 1, priority = 1, description = "this is my first test")
+	public void addRandomItem() throws InterruptedException {
 		Random Rand = new Random();
-		int randmonindex =Rand.nextInt(6);
+		int randmonindex =Rand.nextInt(4);
 		driver.get(URL);
 	
 	WebElement Couniter=	driver.findElement(By.className("product-items"));
@@ -48,10 +51,9 @@ public class Magento {
 			
 			
 			
-			if(driver.getCurrentUrl().equals("fusion")|| driver.getCurrentUrl().contains("push")) {
-				
-				  WebElement Addtocartbutton= driver.findElement(By.id("product-addtocart-button"));
-						Addtocartbutton.click();
+			if (driver.getCurrentUrl().contains("fusion") || driver.getCurrentUrl().contains("push")) {
+				WebElement addtoCartButton = driver.findElement(By.id("product-addtocart-button"));
+				addtoCartButton.click();
 				
 				
 			}
@@ -70,16 +72,61 @@ public class Magento {
 				theListOfColors.get(Randomcolor).click();
 				 WebElement Addtocartbutton= driver.findElement(By.id("product-addtocart-button"));
 				Addtocartbutton.click();
+				Thread.sleep(3000);
 				
 			
+			}}
+			@Test(priority = 2,description = "this is the second test which is checkout")
+			public void Checkoutprocess() throws InterruptedException {
+				String Checkoutpage= "https://magento.softwaretestingboard.com/checkout/cart/";
+				
+				driver.get(Checkoutpage);
+				Thread.sleep(3000);
+				WebElement proceedbutton=driver.findElement(By.xpath("//button[@data-role='proceed-to-checkout']"));
+				proceedbutton.click();
+				System.out.println("alf mbrook");
+				
+				
+			}
+			
+			@Test(priority = 3)
+			public void Signupprocess() {
+				
+				WebElement email= driver.findElement(By.id("customer-email"));
+				WebElement firstname= driver.findElement(By.name("firstname"));
+				WebElement lastname= driver.findElement(By.name("lastname"));
+				WebElement streetadrress= driver.findElement(By.name("street[0]"));
+				WebElement city= driver.findElement(By.name("city"));
+				WebElement state= driver.findElement(By.name("region_id"));
+				WebElement postelcode= driver.findElement(By.name("postcode"));
+				WebElement  country= driver.findElement(By.name("country_id"));
+				WebElement phoneno= driver.findElement(By.name("telephone"));
+				
+				
+				email.sendKeys("muna.jebril1995@hotmail.com");
+				firstname.sendKeys("Muna");
+				lastname.sendKeys("jebril");
+				streetadrress.sendKeys("omar bin alkhttab");
+				city.sendKeys("amman");
+				state.sendKeys("amman");
+				postelcode.sendKeys("1181");
+				country.sendKeys("jordan");
+				phoneno.sendKeys("782431640");
+				WebElement nextbutton = driver.findElement(By.cssSelector(".button.action.continue.primary"));
+				nextbutton.click();
+				
+				Select select = new Select(country);
+				select.selectByVisibleText("jordan");
+				
+				
 			}
 		
 		
-		
+
 	
 							
-						}
 						
+					
 						
 						
 					
